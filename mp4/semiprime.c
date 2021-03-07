057d7181151aeedc3962e3409273ba1d8583c961
Zekai Zhang <zekaiz2@illinois.edu>
2021-03-07 16:50:36 -0600

    Update MP5

--- mp4/semiprime.c
+++ mp4/semiprime.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 
+
 /*
  * is_prime: determines whether the provided number is prime or not
  * Input    : a number
@@ -13,14 +14,20 @@ int is_prime(int number)
     if (number == 1) {return 0;}
     for (i = 2; i < number; i++) { //for each number smaller than it
         if (number % i == 0) { //check if the remainder is 0
-            return 1;
+	   return 0; //here should return 0 because this number is not prime
         }
     }
-    return 0;
+    return 1; //here should return 1 because this number is prime
 }
 
 
 /*
+ * Name: Zekai Zhang (netid: zekaiz2)
+ * Partner: hz39, zw53
+ * Introduction: There are many errors in the provided code. In function is_prime, 
+ * the return value should be flipped. In the semiprime function below, in the second for 
+ * loop, k should be the division of i and j, or it will print incorrect output. Finally, add 
+ * return 1 to make sure the function will return a result.
  * print_semiprimes: prints all semiprimes in [a,b] (including a, b).
  * Input   : a, b (a should be smaller than or equal to b)
  * Return  : 0 if there is no semiprime in [a,b], else 1
@@ -34,9 +41,10 @@ int print_semiprimes(int a, int b)
         for (j = 2; j <= i; j++) {
             if (i%j == 0) {
                 if (is_prime(j)) {
-                    k = i%j;
-                    if (is_prime(k)) {
+		    k = i/j; //should be division instead of modulo
+                    if (is_prime(k) && (k>=j)) { // should be checking if k>=j
                         printf("%d ", i);
+		 ret = 1; 
                     }
                 }
             }
