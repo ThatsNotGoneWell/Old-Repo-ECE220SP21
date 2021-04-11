db889b0eecbcc037e076b1f4962ce871943a8b34
Zekai Zhang <zekaiz2@illinois.edu>
2021-04-11 00:14:59 -0500

    Update Mp9

--- mp9/maze.c
+++ mp9/maze.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "maze.h"
-
+//Name: Zekai Zhang (zekaiz2)
+//Partners: hz39, zw53
+//Partner:
+//Intro Paragraph:
+//The goal of this program is to implement a maze solver by using a recursive depth-first search algorithm//
+//as well as a couple of functions that can be used to verify a correct solution.//
+//The first two numbers indicates the size of the maze which is the width and the height of the maze.//
+//For this program, we are able to find a solution to solve the maze from the starting point("S") to the ending point("E")//
+//In the maze,  ‘%’ represents walls within the maze while a space represents an empty cell.//
+//‘*’ represents the cells that make up the solution for the maze.//
+//and ‘~’ represents the cells that aren't a part of the solution but were visited during the search.//
+//You could only move right, left, up, and down in order to solve the maze.//
 
 /*
  * createMaze -- Creates and fills a maze structure from the given file
@@ -12,8 +23,44 @@
  */
 maze_t * createMaze(char * fileName)
 {
-    // Your code here. Make sure to replace following line with your own code.
-    return NULL;
+	int i,j;
+  char mazeChar;
+  FILE *file;
+  file = fopen(fileName, "r");
+  
+  maze_t * newMaze = malloc(sizeof(maze_t)); // allocate maze_t
+  fscanf(file, "%d %d", &(newMaze->width), &(newMaze->height)); // retrieve width and height from file
+  newMaze->cells = (char**)malloc(newMaze->height * sizeof(char*)); // allocate pointer array
+  
+  // allocate memory for each row
+  for (i=0; i<newMaze->height; i++){
+    newMaze->cells[i] = (char*)malloc(newMaze->width * sizeof(char));
+  }
+  
+  // store each entry of maze to memory
+  for (i=0; i<newMaze->height; i++){ // each row
+  	for (j=0; j<newMaze->width; j++){ // each column
+    	mazeChar = fgetc(file); // get char from file
+      while (mazeChar == '\n'){ // keep getting char until it is not a new line
+      	mazeChar = fgetc(file);
+      }
+    	if (mazeChar == 'S'){ // find starting point
+        newMaze->cells[i][j] = mazeChar;
+        newMaze->startColumn = j;
+        newMaze->startRow = i;
+      }
+      else if (mazeChar == 'E'){ // find ending point
+        newMaze->cells[i][j] = mazeChar;
+        newMaze->endColumn = j;
+        newMaze->endRow = i;
+      }
+      else
+        newMaze->cells[i][j] = mazeChar;
+    }
+  }
+  
+  fclose(file);
+	return newMaze;
 }
 
 /*
@@ -25,7 +72,15 @@ maze_t * createMaze(char * fileName)
  */
 void destroyMaze(maze_t * maze)
 {
-    // Your code here.
+	int i;
+  
+  // free memory starting from each data entry of array
+  for (i=0; i<maze->height; i++){
+  	free(maze->cells[i]);
+  }
+  
+  free(maze->cells); // free pointer array
+  free(maze); // free the structure
 }
 
 /*
@@ -39,7 +94,13 @@ void destroyMaze(maze_t * maze)
  */
 void printMaze(maze_t * maze)
 {
-    // Your code here.
+  int i,j;  
+  for(i=0;i<maze->height;i++){
+    for(j=0;j<maze->width;j++){
+      printf("%c", maze->cells[i][j]);
+    }
+    printf("\n");
+  }
 }
 
 /*
@@ -53,6 +114,47 @@ void printMaze(maze_t * maze)
  */ 
 int solveMazeDFS(maze_t * maze, int col, int row)
 {
-    // Your code here. Make sure to replace following line with your own code.
-    return 0;
+  if(maze->cells[row][col] == 'E'){
+    return 1;//Successful base case: reached destination
+  }
+  
+  if(row<0 || col<0 || row>=maze->height-1 || col>=maze->width-1){
+    return 0;//Unsuccessful base case: out of bounds
+  }
+  
+  if(maze->cells[row][col] != ' ' && maze->cells[row][col] != 'S'){
+    return 0;//Unsuccessful base case: not a valid place
+  }
+  
+  if(maze->cells[row][col] != 'S'){
+  	maze->cells[row][col] = '*';//mark this as a potential solution
+  }
+  
+  if(maze->cells[row-1][col] != 'S'){
+    if(solveMazeDFS(maze,col,(row-1))){
+      return 1;//Recursively check if the right cell is a possible solution
+    }
+  }
+
+  if(maze->cells[row+1][col] != 'S'){
+    if(solveMazeDFS(maze,col,(row+1))){
+      return 1;//Recursively check if the left cell is a possible solution
+    }
+  }
+
+  if(maze->cells[row][col-1] != 'S'){
+    if(solveMazeDFS(maze,(col-1),row)){
+      return 1;//Recursively check if the cell below is a possible solution
+    }
+  }
+  
+  if(maze->cells[row][col+1] != 'S'){
+    if(solveMazeDFS(maze,(col+1),row)){
+      return 1;//Recursively check if the cell above is a possible solution
+    }
+  }
+    
+  maze->cells[row][col] = '~';//mark this as visited
+  
+  return 0;//Everything fails, maze can't be solved
 }
